4e56b45cf57f2d112d7245b229333068c68f60f7
yeondub1121 <kssjshyjj@naver.com>
2024-12-06 17:49:08 +0900

    Feat: 초기설정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import styled from 'styled-components';
-import headerLogo from '../assets/header.svg'; // header.svg 파일 경로에 맞게 수정
+import headerLogo from '../assets/header.svg'; 
 
 const HeaderContainer = styled.header`
-  background-color: #052309;
+  background: linear-gradient(90deg, #052309, #148923);
   color: white;
   top: 0;
   left: 0;
   width: 100%;
-  height: 80px; /* 헤더 높이 조정 */
+  height: 80px; 
   display: flex;
-  align-items: center; /* 수직 중앙 정렬 */
+  align-items: center; 
 `;
 
 const NavContainer = styled.nav`
@@ -20,12 +20,12 @@ const NavContainer = styled.nav`
   width: 100%;
   max-width: 1200px;
   margin: 0 auto;
-  padding: 0 16px; /* 좌우 여백 추가 */
+  padding: 0 16px; 
 `;
 
 const Logo = styled.img`
-  height: 50px; /* 로고 크기 */
-  width: auto; /* 비율 유지 */
+  height: 50px; 
+  width: auto; 
 `;
 
 const NavLinks = styled.ul`
@@ -50,10 +50,9 @@ const Header: React.FC = () => {
   return (
     <HeaderContainer>
       <NavContainer>
-        {/* 로고 이미지 */}
+      
         <Logo src={headerLogo} alt="Header Logo" />
-        
-        {/* 내비게이션 메뉴 */}
+      
         <NavLinks>
           <NavLink>
             <a href="#about">소개</a>
