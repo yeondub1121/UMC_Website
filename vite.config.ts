77018e6eef1bae1da2c6bf0e97e508757d7cbe53
yeondub1121 <kssjshyjj@naver.com>
2024-12-17 15:19:31 +0900

    Fix: 배포 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -7,4 +7,16 @@ export default defineConfig({
   server: {
     open: true, 
   },
-})
+  resolve: {
+    alias: {
+      // styled-components 경로를 ESM 모듈로 설정
+      'styled-components': 'styled-components/dist/styled-components.esm.js',
+    },
+  },
+  build: {
+    rollupOptions: {
+      // styled-components 외부화 해제
+      external: [],
+    },
+  },
+});
